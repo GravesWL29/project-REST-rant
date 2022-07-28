4ce0fdfe97f424506e6c09e0e6357f2c907c7592
Wade Graves <wadegraves940@gmail.com>
2022-07-27 19:04:03 -0500

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -12,24 +12,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
-  
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
+  
 
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -9,27 +9,24 @@ function index (data) {
         <p className="text-center">
           {place.cuisines}
         </p>
-        <img src={place.pic} alt={place.name} />
+        <img src={place.pic} alt={place.name} width="400" height="400"/>
         <p className="text-center">
           Located in {place.city}, {place.state}
         </p>
       </div>
     )
   })
-  
-  return (
-    <Def>
-        <main>
-            <h1>Places to Rant or Rave About</h1>
-            <div className="row">
-              {placesFormatted}
-            </div>
-        </main>
-    </Def>
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              <div className='row'>
+                {placesFormatted}
+              </div>
+              
+          </main>
+      </Def>
   )
-}
-
-
+  }
 
 module.exports = index
-

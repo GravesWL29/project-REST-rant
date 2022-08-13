7ca202735fa6a446cecdb6ece03e8deed8b60171
Wade Graves <wadegraves940@gmail.com>
2022-08-13 10:47:45 -0500

    fixed errors, added seed data and formating

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,37 +1,30 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function index (data) {
-  let placesFormatted = data.places.map((place, index) => {
+function index(data) {
+  let placesFormatted = data.places.map((place) => {
     return (
       <div className="col-sm-6">
         <h2>
-          <a href={`/places/${index}`} >
-            {place.name}
-          </a>
+          <a href={`/places/${place.id}`}>{place.name}</a>
         </h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
-        <img src={place.pic} alt={place.name} />
-        <p className="text-center">
+        <p className="text-sm-left">{place.cuisines}</p>
+        <img className="col-sm-6" src={place.pic} alt={place.name} />
+        <p className="text-sm-left">
           Located in {place.city}, {place.state}
         </p>
       </div>
-    )
-  })
-  
-    return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX PAGE</h1>
-              <div className='row'>
-                {placesFormatted}
-              </div>
-              
-          </main>
-      </Def>
-  )
-  }
+    );
+  });
 
-module.exports = index
+  return (
+    <Def>
+      <main>
+        <h1>Places to Rant or Rave About</h1>
+        <div className="row">{placesFormatted}</div>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = index;

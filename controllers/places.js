f370f60d13e6f2c8053266f4cc093d66f27a6f5a
Wade Graves <wadegraves940@gmail.com>
2022-08-13 11:54:26 -0500

    cot comments to show up

--- controllers/places.js
+++ controllers/places.js
@@ -47,7 +47,9 @@ router.get("/new", (req, res) => {
 //GET BY ID
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
     .then((place) => {
+      console.log(place.comments)
       res.render("places/show", { place });
     })
     .catch((err) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No Comments Yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map (c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant!' : 'Rave! '}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
       <main>
@@ -43,6 +62,7 @@ function show(data) {
         </div>
         <hr />
             <h2>Comments</h2>
+            {comments}
             <div className="row">
               
             </div>
